a6438d0b1bfdea4055f70a3d37fd8c07ae917391
Guankee <1824959631@qq.com>
2023-12-18 14:23:29 +0800

    add 4-4

---
+++ sample_4_4Stencil/shaders/shader.fs
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+struct Material {
+     sampler2D texture_diffuse1; 
+}; 
+uniform Material material;
+
+void main()
+{    
+    FragColor = texture(material.texture_diffuse1, TexCoords);
+}
