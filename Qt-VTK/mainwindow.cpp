81b69be1e6849d722ef45c7af2ddca0de9359017
Guankee <1824959631@qq.com>
2024-06-19 09:13:52 +0800

    实现显示ply

--- Qt-VTK/mainwindow.cpp
+++ Qt-VTK/mainwindow.cpp
@@ -1,14 +1,113 @@
 ﻿#include "mainwindow.h"
+
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  initVTK();
+  setAcceptDrops(true);  // 启用拖放功能
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+void MainWindow::initVTK() {
+  if (nullptr == curVtkcamera) {
+    curVtkcamera = vtkSmartPointer<vtkCamera>::New();
+  }
+  if (nullptr == curVtkRenderer) {
+    curVtkRenderer = vtkSmartPointer<vtkRenderer>::New();
+  }
+  if (nullptr == qvtkInteractor) {
+    qvtkInteractor = vtkSmartPointer<QVTKInteractor>::New();
+  }
+  curVtkRenderer->SetBackground2(0.529, 0.8078,
+                                 0.92157);       // 设置页面底部颜色值
+  curVtkRenderer->SetBackground(1.0, 1.0, 1.0);  // 设置页面顶部颜色值
+  curVtkRenderer->SetGradientBackground(true);
+
+  qvtkInteractor = ui->qVTKOpenglWidget->interactor();
+  curVtkcamera = curVtkRenderer->GetActiveCamera();
+  curentVtkWindow = ui->qVTKOpenglWidget->renderWindow();
+  curentVtkWindow->AddRenderer(curVtkRenderer);
+
+  // lightFront = vtkSmartPointer<vtkLight>::New();
+  // lightFront->SetLightTypeToSceneLight();
+  // curVtkRenderer->AddLight(lightFront);
+
+  // lightBack = vtkSmartPointer<vtkLight>::New();
+  // lightBack->SetLightTypeToSceneLight();
+  // lightBack->SetIntensity(0.5);
+  // // lightBack->SetColor(195.0/255, 176.0/255, 145.0/255);
+  // curVtkRenderer->AddLight(lightBack);
+
+  vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+      vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+  qvtkInteractor->SetRenderWindow(curentVtkWindow);
+  qvtkInteractor->SetInteractorStyle(style);
+  // 用于启动事件处理循环，这在纯 VTK 应用程序中是必需的
+  //  qvtkInteractor->Start();
+  qvtkInteractor->Initialize();
+}
+void MainWindow::addPolyData(vtkSmartPointer<vtkPolyData> polydata,
+                             bool append) {
+  if (!append) {
+    cleanMeshData();
+  }
+  if (nullptr == curPolyData) {
+    curPolyData = vtkSmartPointer<vtkPolyData>::New();
+  }
+  curPolyData = polydata;
+  double *center = polydata->GetCenter();
+  curVtkcamera->SetFocalPoint(center[0], center[1], center[2]);  // 设焦点
+
+  auto mapper = vtkSmartPointer<
+      vtkPolyDataMapper>::New();  // 映射器，可以根据点、线、面构造图形
+  mapper->SetInputData(curPolyData);
+  mapper->Update();
+
+  curactor->SetMapper(mapper);
+  //   ouraud 插值 (SetInterpolationToGouraud)：
+  // 光照计算在每个顶点进行，然后在多边形内部使用线性插值计算光照值。这会产生较为平滑的光照过渡。
+  curactor->GetProperty()->SetInterpolationToGouraud();
+
+  curVtkRenderer->AddActor(curactor);
+    render();
+}
+void MainWindow::cleanMeshData() {
+  vtkSmartPointer<vtkActorCollection> actors = curVtkRenderer->GetActors();
+  actors->InitTraversal();
+  for (int i = 0; i < actors->GetNumberOfItems(); i++) {
+    curVtkRenderer->RemoveActor(actors->GetNextActor());
+  }
+  actors->RemoveAllItems();
+}
+void MainWindow::dragEnterEvent(QDragEnterEvent *event) {
+  if (event->mimeData()->hasUrls()) {
+    QList<QUrl> urlList = event->mimeData()->urls();
+    if (urlList.size() == 1 && urlList[0].toLocalFile().endsWith(".ply")) {
+      event->acceptProposedAction();
+    }
+  }
+}
+
+void MainWindow::dropEvent(QDropEvent *event) {
+  QList<QUrl> urlList = event->mimeData()->urls();
+  if (urlList.size() == 1) {
+    QString filePath = urlList[0].toLocalFile();
+    if (filePath.endsWith(".ply")) {
+      // 在这里处理 .ply 文件，比如显示文件路径
+      vtkSmartPointer<vtkPLYReader> reader =
+          vtkSmartPointer<vtkPLYReader>::New();
+      reader->SetFileName(filePath.toStdString().c_str());
+      reader->Update();
+      vtkSmartPointer<vtkPolyData> polyData = reader->GetOutput();
+      addPolyData(polyData);
+    
+    }
+  }
+}
+void MainWindow::render() {
+    if (curVtkRenderer) {
+        curVtkRenderer->GetRenderWindow()->Render();
+    }
 }
--- Qt-VTK/mainwindow.h
+++ Qt-VTK/mainwindow.h
@@ -2,7 +2,32 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include<QVTKOpenGLNativeWidget.h>
+#include <QList>
+#include <QUrl>
+#include <QMimeData>
+
+//VTK
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+#include <QVTKOpenGLNativeWidget.h>
+#include <QVTKInteractor.h>
+#include <vtkCamera.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderWindow.h>
+#include <vtkLight.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkPolyData.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActorCollection.h>
+#include <vtkProperty.h>
+#include <vtkPLYReader.h>
+//stl
+#include<vector>
+#include<string>
 namespace Ui {
 class MainWindow;
 }
@@ -14,7 +39,25 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+protected:
+   void dragEnterEvent(QDragEnterEvent *event) override;
+    // void dragMoveEvent(QDragMoveEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
 
+  void initVTK();
+  void addPolyData(vtkSmartPointer<vtkPolyData> polydata,bool append=false);
+  void cleanMeshData();
+  void  render();
+private:
+  vtkSmartPointer<vtkRenderer> curVtkRenderer{nullptr};
+  vtkSmartPointer<vtkRenderWindow>curentVtkWindow{nullptr};
+  vtkSmartPointer<vtkCamera>curVtkcamera{nullptr};
+  vtkSmartPointer<QVTKInteractor>qvtkInteractor{nullptr};
+  vtkSmartPointer<vtkLight>lightFront{nullptr};
+  vtkSmartPointer<vtkLight>lightBack{nullptr};
+  // std::vector<vtkPolyData*>polys;
+  vtkSmartPointer<vtkPolyData>curPolyData{nullptr};
+  vtkSmartPointer<vtkActor> curactor = vtkSmartPointer<vtkActor>::New();
 private:
     Ui::MainWindow *ui;
 };
